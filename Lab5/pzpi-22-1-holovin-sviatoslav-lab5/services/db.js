1ec1ceb879198cb1454d764ef955b4825c8b73e1
svyatoslav.golovin.2005@gmail.com <svyatoslav.golovin.2005@gmail.com>
2025-05-11 13:38:26 +0300

    chore(main): fix

--- Lab5/pzpi-22-1-holovin-sviatoslav-lab5/services/db.js
+++ Lab5/pzpi-22-1-holovin-sviatoslav-lab5/services/db.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.PASSWORD,
   {
     host: process.env.HOST,
+    port: process.env.DB_PORT || undefined,
     dialect: 'postgres',
     logging: false,
   },
